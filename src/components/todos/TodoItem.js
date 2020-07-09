89b16697d8a799aec2278b5f9a627ade4746a403
augedoo <augedoo@gmail.com>
2020-07-09 00:29:30 +0000

    Fix: checkbox checkstate load correctly from server to UI but with warning about checkbox state not controlled

--- src/components/todos/TodoItem.js
+++ src/components/todos/TodoItem.js
@@ -9,7 +9,16 @@ function TodoItem({ task, changeTaskStatus }) {
 
   return (
     <div className='flex justify-start items-center px-3 py-2 hover:bg-gray-300'>
-      <input type='checkbox' className='mr-4 h-4 w-4' onChange={onchange} />
+      {task.isCompleted ? (
+        <input
+          type='checkbox'
+          checked
+          className='mr-4 h-4 w-4'
+          onChange={onchange}
+        />
+      ) : (
+        <input type='checkbox' className='mr-4 h-4 w-4' onChange={onchange} />
+      )}
 
       <span className='text-lg'>{task.task}</span>
     </div>
